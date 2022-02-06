426c12f8aa945738e0308c82abc6ea9d670e0b2c
czarek009 <czarek.s2000@wp.pl>
2022-02-06 17:37:03 +0100

    Hold key to move

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+'''
+Składowe projektu:
+ - kod źródłowy zgodny z PEP 8, komentarze;
+ - kod źródłowy powinien być podzielony na moduły;
+ - automatycznie generowana dokumentacja (Sphinx, pydoc, epydoc), zamiast wygenerowanej dokumentacji można umieścić informację o poleceniu, jakiego należy użyć do jej wygenerowania;
+ - testy jednostkowe (unittest/nose/doctest);
+ - skrypt do pakowania aplikacji lub fragmentu, patrz: Distributing Python Modules.
+
+Elementy aplikacji:
+ - interfejs użytkownika (graficzny, webowy, ncurses etc);
+ - trwale przechowywane dane (alchemy, pickle etc);
+ - kod robiący coś w miarę sensownego; 
+'''
 import pygame
 import random
 from copy import deepcopy
 import pickle
+import os
+
 
 class Piece:
 
-	def __init__(self, x, y, shape, color):
+	def __init__(self, x, y, shape):
 
 		self.x = x
 		self.y = y
-		self.color = color
 		self.id = shape
 
 		if shape == 'O':
@@ -78,16 +92,20 @@ class Tetris:
 
 	SPEED       = 250
 	BLOCK_SIZE  = 60
-	PADDING     = BLOCK_SIZE
 	COLS_N      = 10
 	ROWS_N      = 20
-	RIGHT_MENU  = 6*BLOCK_SIZE
+	PADDING     = BLOCK_SIZE + 30
+	RIGHT_BAR   = 5*BLOCK_SIZE
 	GRID_WIDTH  = COLS_N * BLOCK_SIZE
 	GRID_HEIGHT = ROWS_N * BLOCK_SIZE
-	WIN_WIDTH   = GRID_WIDTH + 2*PADDING + RIGHT_MENU
+	WIN_WIDTH   = GRID_WIDTH + 3*PADDING + RIGHT_BAR
 	WIN_HEIGHT  = GRID_HEIGHT + 2*PADDING
 	SHAPES      = ['I', 'T', 'O', 'L', 'J', 'S', 'Z']
 	BACKGROUND_COLOR = (48, 48, 96)
+	BORDER_COLOR     = (32, 32, 64)
+	GRID_COLOR       = (128, 128, 128)
+	TEXT_COLOR       = (200, 200, 200)
+	SCORES_FILE = 'high_scores.pickle'
 
 	def __init__(self):
 
@@ -98,22 +116,36 @@ class Tetris:
 		self.board  = [[(0, 0, 0) for _ in range(self.COLS_N)] for _ in range(self.ROWS_N)]
 		self.score  = 0
 		self.mult   = 1.0
+		
+		if os.path.isfile(self.SCORES_FILE):
+			with open(self.SCORES_FILE, "rb") as file:
+				self.high_scores = pickle.load(file)
+		else:
+			self.high_scores = []
+
+		self.high_scores.sort(key = lambda e: e[1], reverse = True)
+		print(self.high_scores)
 
 	def start_game(self):
-
-		clock      = pygame.time.Clock()
-		fall_speed = self.SPEED
-		fall_time  = 0
-		running    = True
-		piece      = Piece(5, 0, random.choice(self.SHAPES), (0, 0, 0))
-		next_piece = Piece(5, 0, random.choice(self.SHAPES), (0, 0, 0))
+		'''
+		Execute main loop
+		'''
+		clock       = pygame.time.Clock()
+		fall_speed  = self.SPEED
+		fall_time   = 0
+		input_speed = 100
+		input_time  = 0
+		running     = True
+		piece       = Piece(4, 0, random.choice(self.SHAPES))
+		next_piece  = Piece(4, 0, random.choice(self.SHAPES))
 
 		while running:
 
 			self.draw_board(piece, next_piece)
 			
 			# Move piece down
-			fall_time += clock.get_rawtime()
+			fall_time  += clock.get_rawtime()
+			input_time += clock.get_rawtime()
 			clock.tick()
 			if fall_time >= fall_speed:
 				fall_time = 0
@@ -125,12 +157,12 @@ class Tetris:
 					if self.game_over():
 						running = False
 						self.save_score()
-						exit(0)
+						break
 					fall_speed *= .99
 					self.mult *= 1.01
 
 					piece = next_piece
-					next_piece = Piece(5, 0, random.choice(self.SHAPES), (0, 0, 0))
+					next_piece = Piece(4, 0, random.choice(self.SHAPES))
 
 			# Keyboard input
 			for event in pygame.event.get():
@@ -138,14 +170,6 @@ class Tetris:
 					running = False
 
 				if event.type == pygame.KEYDOWN:
-
-					if event.key == pygame.K_LEFT:
-						if self.can_move_left(piece):
-							piece.x -= 1
-
-					if event.key == pygame.K_RIGHT:
-						if self.can_move_right(piece):
-							piece.x += 1
 
 					if event.key == pygame.K_UP:
 						if self.can_rotate(piece, 'right'):
@@ -164,6 +188,16 @@ class Tetris:
 
 					if event.key == pygame.K_ESCAPE:
 						running = False
+			
+			if input_time >= input_speed:
+				input_time = 0
+				keys = pygame.key.get_pressed()
+				if keys[pygame.K_LEFT]:
+					if self.can_move_left(piece):
+						piece.x -= 1
+				if keys[pygame.K_RIGHT]:
+					if self.can_move_right(piece):
+						piece.x += 1
 
 		pygame.display.quit()
 		quit()
@@ -171,14 +205,22 @@ class Tetris:
 	# ---------- LOGIC ---------- # 
 
 	def add_piece2board(self, piece):
+		'''
+		Adds current piece to `self.board`. Returns nothing.
 
+		Arguments:
+		----------
+		piece : piece to add
+		'''
 		for x in range(piece.w):
 			for y in range(piece.h):
 				if piece.shape[y][x] == 1:
 					self.board[piece.y + y][piece.x + x] = piece.color
 
 	def clear_row(self):
-
+		'''
+		Checks if there are any full rows to clear. If so, clears them and move higher elements down. Returns nothing.
+		'''
 		for i in range(self.ROWS_N):
 			if all([x != (0, 0, 0) for x in self.board[i]]):
 				self.board[i] = [(0, 0, 0) for _ in self.board[i]]
@@ -188,16 +230,22 @@ class Tetris:
 				self.score = int(self.score + 1000 * self.mult)
 
 	def game_over(self):
-
+		'''
+		Returns true if any block in the highest row is not empty. False otherwise.
+		'''
 		if any([x != (0, 0, 0) for x in self.board[0]]):
 			return True
+		return False
 
 	def save_score(self):
-		
-		self.myfont2 = pygame.font.SysFont('default', self.BLOCK_SIZE*2)
-		text = self.myfont2.render('GAME OVER', False, (255, 255, 255))
-		self.window.blit(text, (self.PADDING + self.COLS_N//2*self.BLOCK_SIZE - text.get_width()//2, 
-														self.WIN_HEIGHT//2 - text.get_height()//2))
+		'''
+		Ask player for name and save score. Then exit.
+		'''
+		big_font = pygame.font.SysFont('default', self.BLOCK_SIZE*2)
+		text = big_font.render('GAME OVER', True, self.TEXT_COLOR)
+		self.window.blit(text, 
+						 (self.PADDING + (self.COLS_N*self.BLOCK_SIZE - text.get_width())//2, 
+						  (self.WIN_HEIGHT - text.get_height())//2))
 
 
 		running = True
@@ -205,6 +253,7 @@ class Tetris:
 		while running:
 
 			self.tombstone(name)
+			pygame.display.update()
 
 			for event in pygame.event.get():
 
@@ -222,28 +271,16 @@ class Tetris:
 					else:
 						name += event.unicode
 
-		print(name, "score:", self.score)	
+		self.high_scores.append((name, self.score))
+		with open(self.SCORES_FILE, "wb") as file:
+			pickle.dump(self.high_scores, file)
 
 	# ----------- GUI ----------- #
 
-	def tombstone(self, name):
-
-		w = self.BLOCK_SIZE*5
-		h = self.BLOCK_SIZE*2
-		x = self.COLS_N*self.BLOCK_SIZE + 2*self.PADDING
-		y = self.BLOCK_SIZE*8 + 2*self.PADDING
-		pygame.draw.rect(self.window, (32, 32, 80), (x, y, w, h), 0)
-
-		#text = self.myfont.render('Score: ' + str(self.score), False, (200, 200, 200))
-		#self.window.blit(text, (x + self.BLOCK_SIZE//2, y + self.BLOCK_SIZE//2))
-		text = self.myfont.render(name, False, (200, 200, 200))
-		self.window.blit(text, (x + self.BLOCK_SIZE//2, y + self.BLOCK_SIZE//2))
-
-
-		pygame.display.update()
-
 	def draw_board(self, piece, next_piece):
-		
+		'''
+		Draws all interface.
+		'''
 		self.window.fill(self.BACKGROUND_COLOR)
 
 		# draw current piece
@@ -261,45 +298,44 @@ class Tetris:
 		self.draw_grid()
 		self.draw_next_piece(next_piece)
 		self.print_score()
+		self.scoreboard()
 		pygame.display.update()
 
-	def draw_block(self, color, x, y):
-		
-		ltx = self.PADDING + self.BLOCK_SIZE*x
-		lty = self.PADDING + self.BLOCK_SIZE*y
-		pygame.draw.rect(self.window, color, (ltx, lty, self.BLOCK_SIZE, self.BLOCK_SIZE), 0)
-
 	def draw_grid(self):
-		
-		BORDER_COLOR = (32, 32, 64)
-		GRID_COLOR = (128, 128, 128)
-		B = self.PADDING
 		# GRID
-		x = B + self.BLOCK_SIZE
-		y = B
-		for i in range(self.COLS_N - 1):
-			pygame.draw.line(self.window, GRID_COLOR, (x, y), (x, y+self.GRID_HEIGHT))
-			x += self.BLOCK_SIZE
-		x = B
-		y = B + self.BLOCK_SIZE
-		for i in range(self.ROWS_N - 1):
-			pygame.draw.line(self.window, GRID_COLOR, (x, y), (x + self.GRID_WIDTH, y))
-			y += self.BLOCK_SIZE
+		B = self.PADDING
+
+		for i in range(1, self.COLS_N):
+			pygame.draw.line(self.window, 
+							 self.GRID_COLOR, 
+							 (B + i*self.BLOCK_SIZE, B), 
+							 (B + i*self.BLOCK_SIZE, B + self.GRID_HEIGHT))
+
+		for i in range(1, self.ROWS_N):
+			pygame.draw.line(self.window, 
+							 self.GRID_COLOR, 
+							 (B, B + i*self.BLOCK_SIZE), 
+							 (B + self.GRID_WIDTH, B + i*self.BLOCK_SIZE))
 
 		# BORDER
-		pygame.draw.line(self.window, BORDER_COLOR, (B, B), (B, B + self.GRID_HEIGHT), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (B + self.GRID_WIDTH, B), (B + self.GRID_WIDTH, B + self.GRID_HEIGHT), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (B, B), (B + self.GRID_WIDTH, B), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (B, B + self.GRID_HEIGHT), (B + self.GRID_WIDTH, B + self.GRID_HEIGHT), 4)
-
+		pygame.draw.rect(self.window,
+						 self.BORDER_COLOR,
+						 (B, B, self.GRID_WIDTH+4, self.GRID_HEIGHT+4),
+						 4)
+		
 	def draw_next_piece(self, piece):
-
-		text = self.myfont.render('NEXT PIECE:', False, (200, 200, 200))
-		self.window.blit(text, (2*self.PADDING + self.BLOCK_SIZE*self.COLS_N + int(.45*self.BLOCK_SIZE), self.PADDING + int(.2*self.BLOCK_SIZE)))
+		'''
+		Shows box with next piece.
+		'''
+		text = self.myfont.render('NEXT PIECE:', True, self.TEXT_COLOR)
+		self.window.blit(text, 
+						 (2*self.PADDING + self.BLOCK_SIZE*self.COLS_N + int(.45*self.BLOCK_SIZE), 
+						 self.PADDING + int(.2*self.BLOCK_SIZE)))
 
 		shape = piece.id
 		color = piece.color
 
+		# kawałek brzydkiego kodu, żeby każdy element rysował się wyśrodkowany
 		if piece.w%2 == 1:
 			bx = 2*self.PADDING + self.BLOCK_SIZE*self.COLS_N + self.BLOCK_SIZE
 			by = 2*self.PADDING + self.BLOCK_SIZE
@@ -320,26 +356,75 @@ class Tetris:
 				x += self.BLOCK_SIZE
 			y += self.BLOCK_SIZE
 			x = bx
-				
 
-		BORDER_COLOR = (32, 32, 64)
-		GRID_COLOR = (128, 128, 128)
-
-		
 		# box
-		pygame.draw.line(self.window, BORDER_COLOR, (2*self.PADDING + self.COLS_N*self.BLOCK_SIZE, self.PADDING + self.BLOCK_SIZE), 
-																								(2*self.PADDING + self.COLS_N*self.BLOCK_SIZE, self.PADDING + 5*self.BLOCK_SIZE), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (2*self.PADDING + self.COLS_N*self.BLOCK_SIZE + 5*self.BLOCK_SIZE, self.PADDING + self.BLOCK_SIZE), 
-																								(2*self.PADDING + self.COLS_N*self.BLOCK_SIZE + 5*self.BLOCK_SIZE, self.PADDING + 5*self.BLOCK_SIZE), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (2*self.PADDING + self.COLS_N*self.BLOCK_SIZE, self.PADDING + self.BLOCK_SIZE), 
-																								(2*self.PADDING + self.COLS_N*self.BLOCK_SIZE + 5*self.BLOCK_SIZE, self.PADDING + self.BLOCK_SIZE), 4)
-		pygame.draw.line(self.window, BORDER_COLOR, (2*self.PADDING + self.COLS_N*self.BLOCK_SIZE, self.PADDING + 5*self.BLOCK_SIZE), 
-																								(2*self.PADDING + self.COLS_N*self.BLOCK_SIZE + 5*self.BLOCK_SIZE, self.PADDING + 5*self.BLOCK_SIZE), 4)
+		pygame.draw.rect(self.window,
+						 self.BORDER_COLOR,
+						 (2*self.PADDING + self.COLS_N*self.BLOCK_SIZE, self.PADDING + self.BLOCK_SIZE, 
+						  5*self.BLOCK_SIZE, 4*self.BLOCK_SIZE), 
+						 4)
 
 	def print_score(self):
+		'''
+		Shows your current score.
+		'''
+		self.window.blit(self.myfont.render('SCORE: ' + str(self.score), True, self.TEXT_COLOR), 
+		                 (2*self.PADDING + self.BLOCK_SIZE*self.COLS_N, 
+										  self.PADDING + 7*self.BLOCK_SIZE))
 
-		text = self.myfont.render('SCORE: ' + str(self.score), False, (200, 200, 200))
-		self.window.blit(text, (2*self.PADDING + self.BLOCK_SIZE*self.COLS_N, self.PADDING + 7*self.BLOCK_SIZE))
+	def draw_block(self, color, x, y):
+		'''
+		Draws single block on board.
+
+		Arguments:
+		----------
+		color : (R, G, B) color of the block
+		x, y : coordinates of block on board grid
+		'''
+		x = self.PADDING + self.BLOCK_SIZE*x
+		y = self.PADDING + self.BLOCK_SIZE*y
+		pygame.draw.rect(self.window, 
+						 color, 
+						 (x, y, self.BLOCK_SIZE, self.BLOCK_SIZE), 
+						 0)
+
+	def tombstone(self, name):
+		'''
+		Draws textbox to type your name.
+		'''
+		w = self.BLOCK_SIZE*5
+		h = self.BLOCK_SIZE*2
+		x = self.COLS_N*self.BLOCK_SIZE + 2*self.PADDING
+		y = 8*self.BLOCK_SIZE + self.PADDING
+		pygame.draw.rect(self.window, 
+						 (32, 32, 80), 
+						 (x, y, w, h), 
+						 0)
+		text = self.myfont.render(name, True, self.TEXT_COLOR)
+		self.window.blit(text, 
+						 (x + w//2 - text.get_width()//2, 
+						 y + h//2 - text.get_height()//2))
+
+	def scoreboard(self):
+		'''
+		Shows best players.
+		'''
+		x = self.PADDING*2 + self.COLS_N*self.BLOCK_SIZE
+		y = self.PADDING + self.BLOCK_SIZE*10
+		pygame.draw.rect(self.window,
+						 self.BORDER_COLOR,
+						 (x,
+						  y,
+						  self.BLOCK_SIZE*5,
+						  self.BLOCK_SIZE*10),
+						 4)
+		
+		for s in self.high_scores[:10]:
+			text = self.myfont.render(s[0] + ": " + str(s[1]), True, self.TEXT_COLOR)
+			self.window.blit(text, 
+							 (x + 4, 
+							  y + (self.BLOCK_SIZE - text.get_height())//2))
+			y += self.BLOCK_SIZE
 
 	# ----- KEYBOARD  INPUT ----- #
 
@@ -347,7 +432,6 @@ class Tetris:
 		'''
 		Returns True if piece can move one position down. False otherwise.
 		'''
-
 		for x in range(piece.w):
 			for y in range(piece.h):
 				if piece.y + piece.h >= self.ROWS_N:
@@ -362,7 +446,6 @@ class Tetris:
 		'''
 		Returns True if piece can move one position left. False otherwise.
 		'''
-
 		for x in range(piece.w):
 			for y in range(piece.h):
 				if piece.x == 0:
@@ -377,7 +460,6 @@ class Tetris:
 		'''
 		Returns True if piece can move one position right. False otherwise.
 		'''
-
 		for x in range(piece.w):
 			for y in range(piece.h):
 				if piece.x + piece.w >= self.COLS_N:
@@ -389,7 +471,18 @@ class Tetris:
 		return True
 
 	def can_rotate(self, piece, direction):
-		
+		'''
+		Checks if piece can rotate in given direction.
+
+		Arguments:
+		----------
+		piece : current piece
+		direction : 'right' (clockwise) or 'left' (counterclockwise)
+
+		Returns:
+		--------
+		True if piece can rotate. False otherwise.
+		'''		
 		dc_piece = deepcopy(piece)
 		if direction == 'left':
 			dc_piece.rotate()
